6939fa15756afc5cb421aae7f559239bf4b5f1b9
FerRodriguez96 <feer.rodriguez96@gmail.com>
2024-04-21 15:09:20 -0300

    ej_3 v1

--- act_2/ej_3/ej_3.go
+++ act_2/ej_3/ej_3.go
@@ -7,3 +7,38 @@ package main
  	impresión no estén en el orden "¡Hola, mundo!" porque el orden de impresión depende del
  	orden que van terminando las gorutinas.
 */
+
+import (
+	"fmt"
+)
+
+func main(){
+
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	ch3 := make(chan string)
+
+	go func (){
+		ch1 <- "Hola"
+	}()
+
+	go func (){
+		ch2 <- "Mundo"
+	}()
+
+	go func (){
+		ch3 <- "!"
+	}()
+
+
+	for i := 0; i < 3; i++ {
+		select {
+			case msg1 := <-ch1:
+				fmt.Println("Canal 1:", msg1)
+			case msg2 := <-ch2:
+				fmt.Println("Canal 2:", msg2)
+			case msg3 := <-ch3:
+				fmt.Println("Canal 3:", msg3)
+		}
+	}
+}
